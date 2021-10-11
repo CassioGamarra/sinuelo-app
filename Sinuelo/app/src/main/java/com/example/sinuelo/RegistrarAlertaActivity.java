ba66848344005fe6d790c677cb8d22302753da8e
cassiogamarra <cassiogamarra@outlook.com>
2021-10-11 03:30:45 -0300

    Ajuste nos prototipos de tela

---
+++ Sinuelo/app/src/main/java/com/example/sinuelo/RegistrarAlertaActivity.java
@@ -0,0 +1,16 @@
+package com.example.sinuelo;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrarAlerta extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_registrar_alerta);
+  }
+
+ 
+}
